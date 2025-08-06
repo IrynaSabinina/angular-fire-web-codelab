01fd1dc8213fe9481cd1353bfeeca2d5f375a0ba
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2025-08-06 16:09:49 +0200

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyDppc7UOnxlx25D0t88yy0bw_V9NqksZl4',
+    authDomain: 'ai-codelab-test1.firebaseapp.com',
+    projectId: 'ai-codelab-test1',
+    storageBucket: 'ai-codelab-test1.firebasestorage.app',
+    messagingSenderId: '589052657500',
+    appId: '1:589052657500:web:20d1f5c402c93b4c710b99',
+    measurementId: 'G-58SS941BB0',
   },
 };
